556d86e88fe9a61fe8c146491ecdb836f3202259
asan122014-ui <asan122014@gmail.com>
2026-03-01 15:15:39 +0530

    Update authApi.js

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/auth",
+  baseURL: "https://asan-driverapp.onrender.com/api/auth",
 });
 
 export const signupDriver = (data) =>
