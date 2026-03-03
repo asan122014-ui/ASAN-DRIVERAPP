93b2e7205e22cd4f9ddb4218e1c359c108892a85
asan122014-ui <asan122014@gmail.com>
2026-03-03 13:21:30 +0530

    Update Admin.js

--- backend/models/Admin.js
+++ backend/models/Admin.js
@@ -1,8 +1,19 @@
 import mongoose from "mongoose";
 
-const adminSchema = new mongoose.Schema({
-  username: { type: String, unique: true },
-  password: { type: String }
-});
+const adminSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true
+    },
+
+    password: {
+      type: String,
+      required: true
+    }
+  },
+  { timestamps: true }
+);
 
 export default mongoose.model("Admin", adminSchema);
