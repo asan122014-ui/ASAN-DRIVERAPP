450c33994294f63689d8881afc972db9aae55653
asan122014-ui <asan122014@gmail.com>
2026-03-06 23:27:15 +0530

    Update Location.js

--- backend/models/Location.js
+++ backend/models/Location.js
@@ -1,13 +1,43 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
-const locationSchema = new mongoose.Schema({
-  driverId: String,
-  latitude: Number,
-  longitude: Number,
-  timestamp: {
-    type: Date,
-    default: Date.now,
+const locationSchema = new mongoose.Schema(
+  {
+    driver: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Driver",
+      required: true,
+      index: true
+    },
+
+    location: {
+      type: {
+        type: String,
+        enum: ["Point"],
+        default: "Point"
+      },
+      coordinates: {
+        type: [Number], // [longitude, latitude]
+        required: true
+      }
+    },
+
+    speed: {
+      type: Number,
+      default: 0
+    },
+
+    heading: {
+      type: Number,
+      default: 0
+    }
   },
-});
+  { timestamps: true }
+);
 
-module.exports = mongoose.model("Location", locationSchema);
+/* Geospatial index for map queries */
+
+locationSchema.index({ location: "2dsphere" });
+
+const Location = mongoose.model("Location", locationSchema);
+
+export default Location;
