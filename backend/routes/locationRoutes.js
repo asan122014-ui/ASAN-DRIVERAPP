c73ed93584737c2b685315584554e4628fc75bb2
asan122014-ui <asan122014@gmail.com>
2026-03-07 09:59:06 +0530

    Update locationRoutes.js

--- backend/routes/locationRoutes.js
+++ backend/routes/locationRoutes.js
@@ -1,5 +1,3 @@
-import Location from "../models/Location.js";
-
 import express from "express";
 import verifyToken from "../middleware/auth.js";
 import Location from "../models/Location.js";
@@ -22,20 +20,24 @@ router.post("/update", verifyToken, async (req, res) => {
     }
 
     const location = await Location.create({
-      driverId: req.user.id,
-      latitude: lat,
-      longitude: lng
+      driver: req.user.id,
+      location: {
+        type: "Point",
+        coordinates: [lng, lat] // [longitude, latitude]
+      }
     });
 
     /* Emit location to admin dashboard */
 
     const io = req.app.get("io");
 
-    io.emit("driver_location", {
-      driverId: req.user.id,
-      lat,
-      lng
-    });
+    if (io) {
+      io.emit("driver_location", {
+        driverId: req.user.id,
+        lat,
+        lng
+      });
+    }
 
     res.json({
       success: true,
