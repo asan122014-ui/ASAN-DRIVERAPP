7397726465b8c03d836b6798b2197361df0d4a63
asan122014-ui <asan122014@gmail.com>
2026-03-07 12:00:43 +0530

    Update Driver.js

--- backend/models/Driver.js
+++ backend/models/Driver.js
@@ -169,22 +169,18 @@ driverSchema.index({ location: "2dsphere" });
 /* ================= HASH PASSWORD ================= */
 
 driverSchema.pre("save", async function (next) {
-
-  if (!this.isModified("password")) return next();
-
   try {
+    if (!this.isModified("password")) {
+      return next();
+    }
 
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
 
     next();
-
   } catch (error) {
-
     next(error);
-
   }
-
 });
 
 /* ================= PASSWORD COMPARE ================= */
