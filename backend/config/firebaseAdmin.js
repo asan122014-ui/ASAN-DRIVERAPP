83071c17e998ff788af0d1ffe20ad2ed16f940a4
asan122014-ui <asan122014@gmail.com>
2026-04-02 10:06:13 +0530

    Update firebaseAdmin.js

--- backend/config/firebaseAdmin.js
+++ backend/config/firebaseAdmin.js
@@ -3,7 +3,7 @@ import fs from "fs";
 import path from "path";
 
 // 🔥 get absolute path
-const serviceAccountPath = path.resolve("config/firebase-service-account.json");
+const serviceAccountPath = path.resolve("config/asan-app-4b7ea-firebase-adminsdk-fbsvc-1dd8033fce.json");
 
 // 🔥 read JSON file
 const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, "utf-8"));
