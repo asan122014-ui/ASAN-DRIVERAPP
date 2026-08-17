b150a504f38f5eaac4ce217a0bffae731eeaeb9f
asan122014-ui <asan122014@gmail.com>
2026-08-17 23:28:23 +0530

    Remove unused import of verifyAdmin middleware

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,7 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
-import verifyAdmin from "../middleware/verifyAdmin.js";
 
 import {
   loginLimiter,
